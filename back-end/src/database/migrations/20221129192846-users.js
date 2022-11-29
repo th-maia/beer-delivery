2e7d16a44b3cf0c00d6e085e3038761bcaa4a659
asafworld <gabrielasaf10@gmail.com>
2022-11-29 18:20:36 -0300

    Cria Migrations. Commit inicial.
    Co-authored-by: Arthur Coelho <hubarthurcoelho@users.noreply.github.com>
    Co-authored-by: Alvaro Guilherme de Oliveira <Alvruo@users.noreply.github.com>
    Co-authored-by: thiago maia <th-maia@users.noreply.github.com>
    Co-authored-by: Otávio Azevedo <otavioad1997@gmail.com>
    Co-authored-by: matheusfoaraujo <matheusfoaraujo@users.noreply.github.com>

---
+++ back-end/src/database/migrations/20221129192846-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false,
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
