877cffef236e68c5b6ebfd541594021b05e4c826
asafworld <gabrielasaf10@gmail.com>
2022-11-30 17:13:01 -0300

    Cria a rota de registros

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('express-async-errors');
 const { loginRoute } = require('./routes/Login.route');
+const { registerRoute } = require('./routes/Register.route');
 
 const handle = (error, _req, res, next) => {
   res.status(error.status || 500).json({ message: error.message });
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRoute);
+app.use('/register', registerRoute);
 app.use(handle);
 
 module.exports = app;
---
+++ back-end/src/api/services/Register.service.js
@@ -0,0 +1,30 @@
+const { User } = require('../../database/models/index');
+const { generateToken } = require('../utils/JWT');
+const CustomHttpError = require('../middlewares/CustomHttpError');
+const md5 = require('md5');
+
+const postUser = async ({ name, email, password, role }) => {
+  const user = await User.findOne({ where: { email } });
+
+  if (user) {
+    throw new CustomHttpError(409, 'EMAIL ALREADY EXIST');
+  }
+
+  const hash = md5(password);
+
+  const result = await User.create({ name, email, password: hash, role });
+  if (!result) {
+    throw new CustomHttpError(500, 'COULD NOT REGISTER USER');
+  }
+
+  const token = await generateToken({ email: result.email, name: result.name, role: result.role });
+
+  return {
+    name: result.name,
+    email: result.email,
+    role: result.role,
+    token,
+  };
+};
+
+module.exports = { postUser };
