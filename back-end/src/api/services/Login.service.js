4c512bb2b0972a01e24776eea04d2f602d07c860
Alvaro Guilherme de Oliveira <alvarovzp2009@hotmail.com>
2022-12-05 12:58:10 -0300

    ajustes lint

--- back-end/src/api/services/Login.service.js
+++ back-end/src/api/services/Login.service.js
@@ -4,10 +4,9 @@ const { generateToken } = require('../utils/JWT');
 const CustomHttpError = require('../middlewares/CustomHttpError');
 
 const getUser = async (email, pass) => {
-  const hash = md5(pass);
-  const password = hash;
+  const hash = md5(pass); 
   const user = await User.findOne({
-    where: { email, password },
+    where: { email, password: hash },
   });
   if (!user) throw new CustomHttpError(404, 'NOT FOUND');
   const token = await generateToken({
@@ -18,7 +17,6 @@ const getUser = async (email, pass) => {
   });
 
   return {
-    id: user.id,
     name: user.name,
     email: user.email,
     role: user.role,
