ec0f4e0fb4451f19e4cf54ec1747d242ca5121ec
thiago maia <thiago.maia@live.com>
2022-12-06 17:19:55 -0300

    saleProductById

--- back-end/src/api/routes/Sales.route.js
+++ back-end/src/api/routes/Sales.route.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { getSales } = require('../controllers/Sales.controller');
+const { getSales, newSale, getSalesProductsById } = require('../controllers/Sales.controller');
 const checkToken = require('../middlewares/VerifyTokenMiddleware');
 const checkBodySale = require('../middlewares/SaleMiddleware');
-const { newSale } = require('../controllers/Sales.controller');
 
 const salesRoute = express.Router();
 
+salesRoute.get('/:id', checkToken, getSalesProductsById);
 salesRoute.get('/', checkToken, getSales);
-
 salesRoute.post('/', checkToken, checkBodySale, newSale);
+
 
 module.exports = { salesRoute };
