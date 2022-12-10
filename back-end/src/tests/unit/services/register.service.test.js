a3b91a59c207a1cf554b0eceffb679f02da71b71
Otávio Azevedo <otavioad1997@gmail.com>
2022-12-10 19:01:55 -0300

    Teste register camada service

---
+++ back-end/src/tests/unit/services/register.service.test.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { Model } = require('sequelize');
+const { describe } = require('mocha');
+const { clienteRegister, clienteResponse, clienteAlreadyRegistered } = require('../../mocks/users');
+const { postUser } = require('../../../api/services/Register.service');
+const { Error } = require('sequelize');
+
+describe ('Testes da rota register, camada service', () => {
+    describe('Teste se é realizado o cadastro com sucesso', () => {
+        beforeEach(async () => {
+            sinon.stub(Model, 'create').resolves(clienteResponse);
+        });
+        afterEach(() => sinon.restore());
+        it('Deve ser feito cadastro com sucesso e retornar um objeto de usuário com as chaves de registro e token', async () => {
+            const user = await postUser(clienteRegister);
+            expect(user).to.have.keys(clienteResponse);
+        });
+    });
+
+    describe('Teste se o usuário já existe', () => {
+        beforeEach(async () => {
+            sinon.stub(Model, 'create').resolves(clienteResponse);
+        });
+        afterEach(() => sinon.restore());
+        it('Deve ser feito cadastro com sucesso e retornar um objeto de usuário com as chaves de registro e token', async () => {
+            try {
+                const user = await postUser(clienteAlreadyRegistered);
+            } catch (err) {
+                expect(err.message).to.be.deep.equal('EMAIL ALREADY EXIST');
+            }
+        });
+    });
+});
