9e63157185330d9f867da31b1fbd1dbe4a18f4cd
Otávio Azevedo <otavioad1997@gmail.com>
2022-12-14 20:11:00 -0300

    Testes integração /login

---
+++ back-end/src/tests/mocks/login.js
@@ -0,0 +1,47 @@
+
+const customerObject = {
+    id: 3,
+    name: 'Cliente Zé Birita',
+    email: 'zebirita@email.com',
+    password: '1c37466c159755ce1fa181bd247cb925',
+    role: 'customer',
+};
+
+const customerLogin = {
+    email: 'zebirita@email.com',
+    password: '$#zebirita#$'
+}
+
+const customerErrorPassword = {
+    email: 'zebirita@email.com',
+    password: '$#ze'
+}
+
+const customerErrorEmail = {
+    email: 'zebiritaemail.com',
+    password: '$#zebirita#$'
+}
+
+const customer = {
+    id: 3,
+    name: 'Cliente Zé Birita',
+    email: 'zebirita@email.com',
+    role: 'customer',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwicm9sZSI6ImN1c3RvbWVyIiwiaWF0IjoxNjcwNzA3NzUzLCJleHAiOjE2NzI3ODEzNTN9.Ed1sylmghHawx2yojIfOlW_2t8YxkFEuU1GuSl0sl1E'
+};
+
+const customerLoginResponse = {
+    name: 'Cliente Zé Birita',
+    email: 'zebirita@email.com',
+    role: 'customer',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwicm9sZSI6ImN1c3RvbWVyIiwiaWF0IjoxNjcwNzA3NzUzLCJleHAiOjE2NzI3ODEzNTN9.Ed1sylmghHawx2yojIfOlW_2t8YxkFEuU1GuSl0sl1E'
+};
+
+module.exports = { 
+    customerObject,
+    customer, 
+    customerLogin,
+    customerLoginResponse,
+    customerErrorEmail,
+    customerErrorPassword,
+ };
