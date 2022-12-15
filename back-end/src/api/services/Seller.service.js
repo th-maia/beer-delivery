3870b5e0382976fcde9c48d540bb84a025cebf1d
Otávio Azevedo <otavioad1997@gmail.com>
2022-12-15 01:27:47 -0300

    Testes integração /seller

--- back-end/src/api/services/Seller.service.js
+++ back-end/src/api/services/Seller.service.js
@@ -1,8 +1,8 @@
 const { Sale, User } = require('../../database/models/index');
 const CustomHttpError = require('../middlewares/CustomHttpError');
 
-const getSellers = () => {
-  const sellers = User.findAll({ where: { role: 'seller' } });
+const getSellers = async () => {
+  const sellers = await User.findAll({ where: { role: 'seller' } });
   if (!sellers) throw new CustomHttpError(404, 'NO SELLER FOUND');
   return sellers;
 };
